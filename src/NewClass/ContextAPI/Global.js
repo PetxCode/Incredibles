320639e452002a20be6234ceed20022d4310402d
PetxCode <brighterdayscodelab@gmail.com>
2021-02-03 00:06:22 +0100

    Local Storage

--- src/NewClass/ContextAPI/Global.js
+++ src/NewClass/ContextAPI/Global.js
@@ -1,19 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect } from "react";
 import { AppFunction } from "./AppFunction";
 import { v4 as uuid } from "uuid";
 
 const initState = {
-  users: [
-    { id: uuid(), name: "Peter" },
-    { id: uuid(), name: "Osas" },
-    { id: uuid(), name: "Ola" },
-  ],
+  users: localStorage.getItem("users")
+    ? JSON.parse(localStorage.getItem("users"))
+    : [],
+  // users: [
+  //   { id: uuid(), name: "Peter" },
+  //   { id: uuid(), name: "Bukky" },
+  // ],
 };
 
 export const GlobalContext = createContext(initState);
 
 export const GlobalProvide = ({ children }) => {
   const [state, dispatch] = React.useReducer(AppFunction, initState);
+
+  //useEffect for Local Storage
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(state.users));
+  }, [state]);
 
   //Action for Adding new User
 
